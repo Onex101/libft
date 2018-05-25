44e87c6637c29ce60c3ca9f0e0d10db13aadb90e
Xeno RHODA <xrhoda@c1r3s4.wethinkcode.co.za>
2018-05-25 14:38:54 +0200

    Editing putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -12,11 +12,40 @@
 
 #include "libft.h"
 
+static int		ft_intlen(int n)
+{
+	int		i;
+
+	i = 0;
+	if (n == 0)
+	{
+		return (1);
+	}
+	while (n)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+
 void	ft_putnbr(int n)
 {
-	char *str;
+	char str[20];
+	int i;
+	int len;
 
-	str = ft_itoa(n);
+	len = ft_intlen(n);
+	i = 0;
+	if (n < 0)
+		str[-i] = '-';
+	if (n == 0)
+		ft_putstr("0\0");
+	str[len + 1] = '\0';
+	while (n)
+	{
+		str[--len] = n % 10 + 48;
+		n = n / 10;
+	}
 	ft_putstr(str);
-	ft_strdel(&str);
 }
