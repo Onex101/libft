d6d7fbcf40afcefe84800e12d87d80cc3deb25b1
Xeno RHODA <xrhoda@c1r3s4.wethinkcode.co.za>
2018-05-28 07:23:41 +0200

    Added header file, recreated ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,13 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xrhoda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/28 06:34:26 by xrhoda            #+#    #+#             */
+/*   Updated: 2018/05/28 06:34:32 by xrhoda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list *new_list;
+	t_list *tmp_node;
 
-	if (lst != NULL && f != NULL)
+	new_list = f(ft_lstnew(lst->content, lst->size));
+	if (!new_list)
+		return (NULL);
+	tmp_node = new_list;
+	lst = lst->next;
+	while (lst->next)
 	{
-		new_list = f(lst);
-		if (new_list && lst->next)
-			new_list->next = ft_lstmap(lst->next, f);
-		return (new_list);
+		tmp_node = f(ft_lstnew(lst->content, lst->size));
+		if (!tmp_node)
+			return (NULL);
+		lst = lst-next;
 	}
-	return (NULL);
+	return (new_list);
 }
